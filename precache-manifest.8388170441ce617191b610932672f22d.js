7b94547bd8cae7f5c9c5a277e206558a24b28221
SebastianLabadie <sebastianlabadie.contact@gmail.com>
2020-09-08 21:38:29 -0300

    Updates

---
+++ precache-manifest.8388170441ce617191b610932672f22d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533990f85a0671eb73398225c703e8d6",
+    "url": "/react-movieSX/index.html"
+  },
+  {
+    "revision": "f3c64e8ec79a9bf519ae",
+    "url": "/react-movieSX/static/css/2.a689a8c1.chunk.css"
+  },
+  {
+    "revision": "a5ba8b5265f96419163c",
+    "url": "/react-movieSX/static/css/main.0600dac7.chunk.css"
+  },
+  {
+    "revision": "f3c64e8ec79a9bf519ae",
+    "url": "/react-movieSX/static/js/2.156bf45b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-movieSX/static/js/2.156bf45b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5ba8b5265f96419163c",
+    "url": "/react-movieSX/static/js/main.4ad0bb98.chunk.js"
+  },
+  {
+    "revision": "837f2fd83206e48634a8",
+    "url": "/react-movieSX/static/js/runtime-main.d87ed8ce.js"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/react-movieSX/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/react-movieSX/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/react-movieSX/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "f86d9bf34b5da8610c6a218e96667b68",
+    "url": "/react-movieSX/static/media/react-bootstrap-carousel.f86d9bf3.svg"
+  }
+]);
